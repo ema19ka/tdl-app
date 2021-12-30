07cf071711d0e980330faf746f9ac647109da834
Eva <it211506@fhstp.ac.at>
2021-12-30 10:06:21 +0100

    BE: config users package

--- backend-server/src/main.ts
+++ backend-server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // const config = new DocumentBuilder()
+  //   .setTitle('yet another to-do-list app')
+  //   .setDescription('Descript')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('swagger', app, document);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  await app.listen(3000).then((d) => console.log('running'));
 }
 bootstrap();
