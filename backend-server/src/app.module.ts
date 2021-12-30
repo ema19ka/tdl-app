53ceed30d75a6e7d256fb970be59eb7928e5a080
Eva <it211506@fhstp.ac.at>
2021-12-30 11:13:40 +0100

    Revert "added typeorm"
    This reverts commit cfa710cb21e1d42973dd1689067bb59a72c9855f.

--- backend-server/src/app.module.ts
+++ backend-server/src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_tdl',
-      entities: ['dist/**/*.entity{.ts,.js'],
-      synchronize: true,
-      logging: true,
-      dropSchema: true,
-    }),
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
