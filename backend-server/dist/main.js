9d341be1caca0ee102110fd073406efc16a27f0a
Eva <it211506@fhstp.ac.at>
2022-01-07 09:44:06 +0100

    setup swagger

--- backend-server/dist/main.js
+++ backend-server/dist/main.js
@@ -3,8 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('TDL')
+        .setDescription('Yes, another to-do-list app')
+        .setVersion('1.0')
+        .addTag('tdl')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('swagger', app, document);
     app.use(cookieParser());
     app.enableCors();
     await app.listen(3000).then((d) => console.log('backend running'));
--- backend-server/src/main.ts
+++ backend-server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'; // alte library braucht diesen import
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TDL')
+    .setDescription('Yes, another to-do-list app')
+    .setVersion('1.0')
+    .addTag('tdl')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   app.use(cookieParser());
   app.enableCors();
