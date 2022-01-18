a57269356e4605b7373810d8cb580db6f7cb74d3
Eva <it211506@fhstp.ac.at>
2022-01-18 11:54:18 +0100

    fe: category form no logic

--- frontend-client/src/app/pages/category/category.page.ts
+++ frontend-client/src/app/pages/category/category.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Category } from 'src/app/services/Category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
+  myForm: FormGroup;
+  submitted = false;
+  public category: Category;
 
-  constructor() { }
+  constructor(public categoryService: CategoryService, private route: Router, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+
+  onAddCategory(){
+    // console.log(this.category.name);
+    this.submitted = true;
+    if (!this.myForm.valid) {
+      console.log('All fields are required.');
+      return false;
+    } else {
+      console.log(this.myForm.value);
+    }
   }
 
 }
--- frontend-client/src/app/services/category.service.ts
+++ frontend-client/src/app/services/category.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Category } from './Category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  public currentCategory: Category = {
+    id: '1',
+    name: 'Name',
+  };
+
+  // constructor() {
+  //   this.load();
+  //  }
+
+  //  addCategory() {
+  //    Storage.set({
+  //      key: 'TDL',
+  //      value: JSON.stringify(this.currentCategory)
+  //    });
+  //    console.log('save');
+  //  }
 }
