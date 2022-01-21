e5495b6b2346ee990508284c0d6992c1f63a3317
Eva <it211506@fhstp.ac.at>
2022-01-21 09:56:34 +0100

    fe: dummy routing

--- backend-server/dist/app.module.js
+++ backend-server/dist/app.module.js
@@ -14,6 +14,7 @@ const app_service_1 = require("./app.service");
 const users_module_1 = require("./users/users.module");
 const auth_module_1 = require("./auth/auth.module");
 const categories_module_1 = require("./categories/categories.module");
+const lists_module_1 = require("./lists/lists.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
@@ -36,6 +37,7 @@ AppModule = __decorate([
             }),
             auth_module_1.AuthModule,
             categories_module_1.CategoriesModule,
+            lists_module_1.ListsModule,
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
---
+++ backend-server/dist/lists/entity/List.entity.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=List.entity.js.map
