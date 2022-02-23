a9af44c83e8cd85f6cb10499c8b332dd855d2be4
Eva <it211506@fhstp.ac.at>
2022-02-23 15:08:31 +0100

    Revert "revert pwa"
    This reverts commit e73dba0130e6a4eab9b90f96955d10360de7297a.

--- frontend-client/src/app/app.module.ts
+++ frontend-client/src/app/app.module.ts
@@ -9,13 +9,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 // import { RegisterComponent } from './register/register.component';
 // import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HttpClientModule],
+  // eslint-disable-next-line max-len
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
