6aab34d2a803c5c1f4c161327ad039fd6a2f6016
Eva <evamaria.altenburger@gmail.com>
2022-06-07 10:34:12 +0200

    Add tab bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  check: boolean;
   constructor() {}
+
+  ngOnInit() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus() {
+    this.check = true;
+    if (this.check.toString() === localStorage.getItem('isLoggedIn')) { 
+      return true;
+    }
+    else {
+      this.check = false;
+      return false;
+    }
+  }
 
 }
